ba0682d75890d2feb9aea7a5c097a7d84428cfc0
sakurakotubaki <gitonly543@gmail.com>
2026-01-29 21:27:56 +0900

    Initial Commit

---
+++ BrainLog/BrainLogApp.swift
@@ -0,0 +1,32 @@
+//
+//  BrainLogApp.swift
+//  BrainLog
+//
+//  Created by 橋本純一 on 2026/01/29.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BrainLogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
